6d246bd483fd95ad62f376a81a8a9baaca78ae54
huthaifa-abd <info@huthaifa-abd.com>
2020-06-09 21:39:04 +0200

    [Automation] Add development env init scripts

---
+++ initdev.bat
@@ -0,0 +1,5 @@
+echo off
+ECHO Initialize Development Environment PROJECT NIGHTOWL V0.1
+ECHO Initialize Video Processing Unit Dependencies
+mkdir VideoProcessingUnit
+git clone https://github.com/ageitgey/face_recognition.git VideoProcessingUnit
