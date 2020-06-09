b0f55d3f9f9282400caa50c7b4e34f8b879cc2e9
huthaifa-abd <info@huthaifa-abd.com>
2020-06-09 22:12:30 +0200

    [Design] Update Component Name

--- initdev.bat
+++ initdev.bat
@@ -3,3 +3,5 @@ ECHO Initialize Development Environment PROJECT NIGHTOWL V0.1
 ECHO Initialize Video Processing Unit Dependencies
 mkdir VideoProcessingUnit
 git clone https://github.com/ageitgey/face_recognition.git VideoProcessingUnit
+cd VideoProcessingUnit
+docker-compose up --build
