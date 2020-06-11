b6b48c5432812b24b23dc9fe7341fc88c11a9e4a
huthaifa-abd <info@huthaifa-abd.com>
2020-06-11 20:58:36 +0200

    [Automation] Update dev init
    Support injecting the video processing agent.

--- initdev.bat
+++ initdev.bat
@@ -3,5 +3,7 @@ ECHO Initialize Development Environment PROJECT NIGHTOWL V0.1
 ECHO Initialize Video Processing Unit Dependencies
 mkdir VideoProcessingUnit
 git clone https://github.com/ageitgey/face_recognition.git VideoProcessingUnit
+xcopy "initvpu.sh" "VideoProcessingUnit" /S
 cd VideoProcessingUnit
 docker-compose up --build
+docker run -it --privileged face_recognition /bin/bash -c "git clone -b 'develop' https://github.com/huthaifa-abd/Project-Nightowl.git && python Project-Nightowl/Components/video-stream-agent/video_stream_agent.py"
