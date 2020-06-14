b1b76be06f21196a4db57cb82997f335cee17e81
huthaifa-abd <info@huthaifa-abd.com>
2020-06-15 00:32:49 +0200

    [Configuration] Application Gateway
    Configuring the application gateway

--- initdev.bat
+++ initdev.bat
@@ -1,5 +1,10 @@
 echo off
 ECHO Initialize Development Environment PROJECT NIGHTOWL V0.1
+ECHO Initialize Eurka Application Gateway Service
+mkdir applicationGateway
+git clone https://github.com/huthaifa-abd/jhipster-registry.git applicationGateway
+ECHO Starting Eurka Application Gateway Service (This might take a while)...
+docker-compose -f Components\applicationGateway\src\main\docker\jhipster-registry.yml up -d
 ECHO Initialize Video Processing Unit Dependencies
 ECHO Important: Dont forget to start ftp server on localhost:21 and configure your cameras to save videos to it
 mkdir VideoProcessingUnit
@@ -8,3 +13,4 @@ xcopy "initvpu.sh" "VideoProcessingUnit" /S
 cd VideoProcessingUnit
 docker-compose up --build
 docker run -it --privileged face_recognition /bin/bash -c "git clone -b 'develop' https://github.com/huthaifa-abd/Project-Nightowl.git && python Project-Nightowl/Components/video-stream-agent/video_stream_agent.py"
+
