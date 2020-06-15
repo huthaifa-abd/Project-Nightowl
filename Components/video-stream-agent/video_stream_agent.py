ad4f94b5a095b0c2d480a53443d6e1b5a8b2ab04
huthaifa-abd <info@huthaifa-abd.com>
2020-06-15 22:25:33 +0200

    [Code] Configure Eurka Client

--- Components/video-stream-agent/video_stream_agent.py
+++ Components/video-stream-agent/video_stream_agent.py
@@ -1,7 +1,13 @@
+import subprocess
+import sys
 import time
 print("Project Nightowl v0.1")
 print("Video Stream Agent v0.1")
+print("initializing required python packages...")
+install("py_eureka_client")
 print("Establishing connection to HDFS...")
 while True:
     print("Waiting for video stream.")
     time.sleep(60) # Delay for 1 minute (60 seconds).
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
