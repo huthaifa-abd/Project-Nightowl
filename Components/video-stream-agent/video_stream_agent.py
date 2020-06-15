d47d0ede77ba8555aae064cdb22f3e404c143457
huthaifa-abd <info@huthaifa-abd.com>
2020-06-15 22:28:08 +0200

    [Code] Update Package Install

--- Components/video-stream-agent/video_stream_agent.py
+++ Components/video-stream-agent/video_stream_agent.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import time
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 print("Project Nightowl v0.1")
 print("Video Stream Agent v0.1")
 print("initializing required python packages...")
@@ -9,5 +11,4 @@ print("Establishing connection to HDFS...")
 while True:
     print("Waiting for video stream.")
     time.sleep(60) # Delay for 1 minute (60 seconds).
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
